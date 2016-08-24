4671b49f56b2edf99bd348bf7b682cfc57d247b5
NinaSleephead <kx1214@gmail.com>
2016-08-23 21:50:25 -0400

    Position-CSS-jQuery-Practice
    Practice for position CSS in jQuery

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*Nina Kong*/
+function value() {
+    $('#width').html($('.Position').width());
+    $('#height').html($('.Position').height());
+    $('#innerWidth').html($('.Position').innerWidth());
+    $('#innerHeight').html($('.Position').innerHeight());
+    $('#outWidth').html($('.Position').outerWidth());
+    $('#outHeight').html($('.Position').outerHeight());
+    $('#offset').html($('.Position').offset().top + "," + $('.Position').offset().left);
+    $('#position').html($('.Position').position().top + "," + $('.Position').position().left);
+}
+function updateValue() {
+    $('.Position').width(500);
+    $('.Position').height(500);
+    value();
+}
+$(document).ready(function () {
+    var windowHeight = $( window ).height();
+    var con = $(".Content");
+    con.parentsUntil($("body")).css("height", windowHeight);
+
+    value();
+    $('.Position').click(updateValue);
+});
